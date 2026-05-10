7ec830588c5d7e5074efe9979df98797fbf77899
Huzaifa Asghar <huzaifadoc123@gmail.com>
2026-05-10 20:14:34 +0500

    Project File

---
+++ Project file.py
@@ -0,0 +1,17 @@
+print("The marks of the students are as follows:")
+
+
+print("\nSafwan: ")
+Safwan = open("Safwan.txt", "r")
+print(Safwan.read())
+Safwan.close()
+
+print("\nHuzaifa: ")
+Huzaifa = open("Huzaifa.txt", "r")
+print(Huzaifa.read())
+Huzaifa.close()
+
+print("\nAli: ")
+Ali = open("Ali.txt", "r")
+print(Ali.read())
+Ali.close()
