1bff8bcfcc8f0c89aaaef6d6386209db437d14aa
Huzaifa Asghar <huzaifadoc123@gmail.com>
2026-05-17 09:08:40 +0500

    Appending and merging

--- Huzaifa.py
+++ Huzaifa.py
@@ -2,16 +2,35 @@ file = open("Huzaifa.txt", "r")
 print(file.read())
 file.close()
 
+print("\n Number of words in Huzaifa.txt are:")
 
-file1 = open("Huzaifa.txt", "a")
-file1.write("\n  I am appending this file because I want to. YOU HAVE A PROBLEM WITH THAT?!!!!!!!!!!!!!!!!")
-file1.close()
+with open("Huzaifa.txt", "r") as file1:
+    data = file1.read()
+    print("Words in this file are...")
+    for line in data:
+        word = line.split()
+        print(word)
 
+print("\n Appending to the file Huzaifa.txt;")
 
-file2 =open('Huzaifa.txt', 'r')
-print(file2.read())
+file2 = open("Huzaifa.txt", "a")
+file2.write("\n  I am appending this file because I want to. YOU HAVE A PROBLEM WITH THAT?!!!!!!!!!!!!!!!!")
 file2.close()
 
-file3 = open('Huzaifa.txt', 'w')
-file3.write(" Hello I am Huzaifa.")
-file3.close()
+
+with open("Condigal.txt", "r") as file3:
+    data1 = file3.read()
+with open("Ali.txt","r") as file3:
+    data2 = file3.read()
+data1 += "\n"
+data1 += data2
+print("Merging two files.....Condigal.txt and Ali.txt")
+with open("MergedFile.txt", "w") as file4:
+    file4.write(data1)
+    print("Merged file created successfully")    
+        
+
+
+
+
+
